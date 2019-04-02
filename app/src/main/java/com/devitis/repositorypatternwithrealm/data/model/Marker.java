6ed0f2085409c62728409dd66a2483012274a2ef
Diana <diana.sinenchenko@gmail.com>
2019-04-02 17:38:39 +0300

    Initial commit

---
+++ app/src/main/java/com/devitis/repositorypatternwithrealm/data/model/Marker.java
@@ -0,0 +1,60 @@
+package com.devitis.repositorypatternwithrealm.data.model;
+
+import java.util.List;
+
+/**
+ * Created by Diana on 02.04.2019.
+ */
+
+public class Marker {
+
+    private int id;
+
+    private String name;
+
+    private String dateAdd;
+
+    private String dateEnd;
+
+    private List<String> location;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateAdd() {
+        return dateAdd;
+    }
+
+    public void setDateAdd(String dateAdd) {
+        this.dateAdd = dateAdd;
+    }
+
+    public String getDateEnd() {
+        return dateEnd;
+    }
+
+    public void setDateEnd(String dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+
+    public List<String> getLocation() {
+        return location;
+    }
+
+    public void setLocation(List<String> location) {
+        this.location = location;
+    }
+}
