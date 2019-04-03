7dfd89bb0a58d2bfef5009561131c785d3aa67f9
Diana <diana.sinenchenko@gmail.com>
2019-04-03 11:05:05 +0300

    add realmMarkerToMarker and markerToRealmMarker

---
+++ app/src/main/java/com/devitis/repositorypatternwithrealm/data/converter/RealmModelMarkerConverter.java
@@ -0,0 +1,44 @@
+package com.devitis.repositorypatternwithrealm.data.converter;
+
+import android.support.annotation.Nullable;
+
+import com.devitis.repositorypatternwithrealm.data.model.Marker;
+import com.devitis.repositorypatternwithrealm.data.realm.RealmMarker;
+import com.devitis.repositorypatternwithrealm.data.utils.StringUtils;
+
+/**
+ * Created by Diana on 03.04.2019.
+ */
+
+public class RealmModelMarkerConverter extends MainRealmModelConverter {
+
+
+    @Nullable
+    public Marker realmMarkerToMarker(@Nullable RealmMarker realmMarker) {
+
+        Marker marker = null;
+        if (realmMarker != null) {
+            marker = new Marker();
+            marker.setId(realmMarker.getId());
+            marker.setName(StringUtils.changeNullToNullString(realmMarker.getName()));
+            marker.setDateAdd(StringUtils.changeNullToNullString(realmMarker.getDateAdd()));
+            marker.setDateEnd(StringUtils.changeNullToNullString(realmMarker.getDateEnd()));
+            marker.setLocation(realmListToList(realmMarker.getLocations()));
+        }
+        return marker;
+    }
+
+    @Nullable
+    public RealmMarker markerToRealmMarker(@Nullable Marker marker) {
+        RealmMarker realmMarker = null;
+        if (marker != null) {
+            realmMarker.setId(marker.getId());
+            realmMarker.setName(StringUtils.changeNullToNullString(marker.getName()));
+            realmMarker.setDateAdd(StringUtils.changeNullToNullString(marker.getDateAdd()));
+            realmMarker.setDateEnd(StringUtils.changeNullToNullString(marker.getDateEnd()));
+            realmMarker.setLocations(listToRealmList(marker.getLocation()));
+        }
+        return realmMarker;
+    }
+
+}
---
+++ app/src/main/java/com/devitis/repositorypatternwithrealm/data/utils/StringUtils.java
@@ -0,0 +1,18 @@
+package com.devitis.repositorypatternwithrealm.data.utils;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Diana on 03.04.2019.
+ */
+
+public final class StringUtils {
+
+    public static final String NULL_STRING = "";
+
+    @NonNull
+    public static String changeNullToNullString(@Nullable String nulll) {
+        return nulll == null ? NULL_STRING : nulll;
+    }
+}
