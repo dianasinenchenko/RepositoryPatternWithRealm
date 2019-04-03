0e3ba10e151f21ecddc7d299a72d0647922b3488
Diana <diana.sinenchenko@gmail.com>
2019-04-03 12:26:06 +0300

    refactoring MarkerRepository

--- app/src/main/java/com/devitis/repositorypatternwithrealm/data/repository/MarkerRepository.java
+++ app/src/main/java/com/devitis/repositorypatternwithrealm/data/repository/MarkerRepository.java
@@ -1,10 +1,20 @@
 package com.devitis.repositorypatternwithrealm.data.repository;
 
+import android.media.MediaActionSound;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
+import com.devitis.repositorypatternwithrealm.data.converter.RealmModelMarkerConverter;
 import com.devitis.repositorypatternwithrealm.data.model.Marker;
+import com.devitis.repositorypatternwithrealm.data.realm.RealmMarker;
+import com.devitis.repositorypatternwithrealm.data.sourse.MarkerDataSource;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmModel;
+import io.realm.RealmResults;
 
 /**
  * Created by Diana on 02.04.2019.
@@ -13,43 +23,105 @@ import java.util.List;
 public class MarkerRepository implements IMainRepository {
 
 
+    private MarkerDataSource dataSource;
+    private RealmModelMarkerConverter converter;
+
+    public MarkerRepository(@NonNull MarkerDataSource dataSource, @NonNull RealmModelMarkerConverter converter) {
+        this.dataSource = dataSource;
+        this.converter = converter;
+    }
+
+    @Nullable
+    private List<Marker> transformRealResultsToList(@Nullable final RealmResults<RealmMarker> realmContactList, @NonNull List<Marker> list) {
+        if (realmContactList != null) {
+            for (RealmMarker realmMarker : realmContactList) {
+                list.add(converter.realmMarkerToMarker(realmMarker));
+            }
+        }
+        return list;
+    }
+
+    @Nullable
     @Override
     public List<Marker> getMarkers() {
-        return null;
+
+        List<Marker> list;
+        Realm realm = Realm.getDefaultInstance();
+        list = new ArrayList<>();
+        RealmResults<RealmMarker> realmMarkerList = dataSource.getMarkers(realm);
+        transformRealResultsToList(realmMarkerList, list);
+
+        return list;
+    }
+
+    @Nullable
+    @Override
+    public Marker getMarkerById(final int id) {
+
+        Marker marker;
+        Realm realm = Realm.getDefaultInstance();
+        marker = converter.realmMarkerToMarker(dataSource.getMarkerById(realm, id));
+
+        return marker;
+    }
+
+    @Nullable
+    @Override
+    public List<Marker> getMarkerByName(@NonNull final String name) {
+
+        List<Marker> list;
+        Realm realm = Realm.getDefaultInstance();
+        list = new ArrayList<>();
+        RealmResults<RealmMarker> realmMarkerListByName = dataSource.getMarkerByName(realm, name);
+        transformRealResultsToList(realmMarkerListByName, list);
+
+        return list;
+    }
+
+
+    @Override
+    public List<Marker> getMarkerByEndDate(@NonNull final String endDate) {
+
+        List<Marker> list;
+        Realm realm = Realm.getDefaultInstance();
+        list = new ArrayList<>();
+        RealmResults<RealmMarker> realmMarkerListByEndDate = dataSource.getMarkerByEndDate(realm, endDate);
+        transformRealResultsToList(realmMarkerListByEndDate, list);
+
+        return list;
     }
 
     @Override
-    public Marker getMarkerById(int id) {
-        return null;
+    public List<Marker> getMarkerByLocation(@NonNull final String location) {
+
+        List<Marker> list;
+        Realm realm = Realm.getDefaultInstance();
+        list = new ArrayList<>();
+        RealmResults<RealmMarker> realmMarkerListByLocation = dataSource.getMarkerByLocation(realm, location);
+        transformRealResultsToList(realmMarkerListByLocation, list);
+
+        return list;
     }
 
     @Override
-    public List<Marker> getMarkerByName(@NonNull Marker name) {
-        return null;
+    public void createMarker(@NonNull final Marker marker) {
+
+        Realm realm = Realm.getDefaultInstance();
+        dataSource.createMarker(realm, converter.markerToRealmMarker(marker));
     }
 
     @Override
-    public List<Marker> getMarkerByEndDate(@NonNull Marker endDate) {
-        return null;
-    }
+    public void updateMarker(@NonNull final Marker marker) {
 
-    @Override
-    public List<Marker> getMarkerByLocation(@NonNull Marker location) {
-        return null;
-    }
-
-    @Override
-    public void createMarker(@NonNull Marker marker) {
-
-    }
-
-    @Override
-    public void updateMarker(@NonNull Marker marker) {
+        Realm realm = Realm.getDefaultInstance();
+        dataSource.updateMarker(realm, converter.markerToRealmMarker(marker));
 
     }
 
     @Override
     public void deleteMarkerById(int markerId) {
 
+        Realm realm = Realm.getDefaultInstance();
+        dataSource.deleteById(realm, markerId);
     }
 }
